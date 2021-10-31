5c3f132e00fce32ba3d577095f43d64a0e1cb3c7
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2021-10-31 16:40:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace atv01_idade
             int idade;
             Console.WriteLine("Qual é sua idade ?");    
              idade = Convert.ToInt16(Console.ReadLine());
-
+            
+            Console.WriteLine("");//Espaço
+            
             if (idade >= 18)
             {
                 Console.WriteLine("Você tem permissão para dirigir");
